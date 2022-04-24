d9d2ea92b647fc77b93896f897ca98f6b8223d61
LuiPJ <mister.keyn@mail.ru>
2022-04-24 20:45:00 +0300

    TS1 init project

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { createHeader, formatTime, getDate } from './utils';
+document.body.appendChild(createHeader(formatTime(getDate())));
+export default 0;
+//# sourceMappingURL=main.js.map
---
+++ js/untils.js
@@ -0,0 +1,9 @@
+const formatter = new Intl.DateTimeFormat("en-US");
+export const formatTime = (date) => formatter.format(date);
+export const createHeader = (title) => {
+    const h1 = document.createElement("h1");
+    h1.innerText = title;
+    return h1;
+};
+export const getDate = () => new Date();
+//# sourceMappingURL=untils.js.map
